2c93d19d0217372c713f1233997604ac9c5a785e
matthiasrsl <mbluesblack01@gmail.com>
2019-11-02 21:41:03 +0100

    Initial commit.

---
+++ README.md
@@ -0,0 +1,5 @@
+# DeezHistory
+
+## Upcoming models schema
+
+![Models class uml](docs/models_class_uml.png)
---
+++ muzhistory/local_settings.py
@@ -0,0 +1,36 @@
+# -*- mode: python; indent-tabs-mode: nil; tab-width: 4 -*- 
+
+import os
+import environ
+
+# Build paths inside the project like this: os.path.join(BASE_DIR, ...)
+BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+
+env = environ.Env()
+env.read_env(env.str('ENV_PATH', os.path.join(BASE_DIR, '.env')))
+
+# SECURITY WARNING: keep the secret key used in production secret!
+SECRET_KEY = env('SECRET_KEY')
+
+INSTALLED_APPS = [
+    'django.contrib.auth',
+    'django.contrib.admin',
+    'django.contrib.contenttypes',
+    'django.contrib.sessions',
+    'django.contrib.messages',
+    'django.contrib.staticfiles',
+    'django.contrib.humanize',
+    #'history',
+]
+
+# False if not in os.environ
+DEBUG = env('DEBUG')
+
+ALLOWED_HOSTS = ['deezhistory.pythonanywhere.com', 'localhost']
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+    }
+}
