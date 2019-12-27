32e489f8d4bf35c914a04a4d3de2050e2e29eb34
matthiasrsl <47864957+matthiasrsl@users.noreply.github.com>
2019-12-27 21:20:19 +0100

    Wrote tests for data retrieval.

--- deezerdata/tests.py
+++ deezerdata/tests.py
@@ -1,3 +1,32 @@
 from django.test import TestCase
 
-# Create your tests here.
+from .models import *
+from platform_apis.models import DeezerApiError
+
+class DeezerAlbumTest(TestCase):
+    def test_retrieve_non_existent(self):
+        """
+        Checks that the retrieval of an album with an invalid deezer id
+        raises a DeezerApiError.
+        """
+        with self.assertRaises(DeezerApiError):        
+            album, created = DeezerAlbum.retrieve(-1)
+            
+            
+class DeezerTrackTest(TestCase):
+    def test_retrieve_non_existent(self):
+        """
+        Checks that the retrieval of an track with an invalid deezer id
+        raises a DeezerApiError.
+        """
+        with self.assertRaises(DeezerApiError):        
+            track, created = DeezerTrack.retrieve(0)
+            
+    def test_retrieve_not_authorized(self):
+        """
+        Checks that the retrieval of a user mp3 without oauth authentication
+        raises a DeezerApiError.
+        """
+        with self.assertRaises(DeezerApiError):        
+            track, created = DeezerTrack.retrieve(-2834538522)
+    
--- musicdata/tests.py
+++ musicdata/tests.py
@@ -1,3 +1,14 @@
 from django.test import TestCase
 
-# Create your tests here.
+from .models import *
+from platform_apis.models import DeezerApiError
+
+class ArtistTest(TestCase):
+    def test_retrieve_from_deezer_non_existent(self):
+        """
+        Checks that the retrieval of an artist with an invalid deezer id
+        raises a DeezerApiError.
+        """
+        with self.assertRaises(DeezerApiError):        
+            artist, created = Artist.retrieve_from_deezer(-1)
+        
