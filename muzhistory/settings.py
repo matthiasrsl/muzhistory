7afa2aae6b825636bd9142bb991867580b92a265
matthiasrsl <47864957+matthiasrsl@users.noreply.github.com>
2020-01-12 20:28:00 +0100

    Set DEBUG to False in production settings.

--- muzhistory/settings.py
+++ muzhistory/settings.py
@@ -29,7 +29,7 @@ env.read_env(env.str('ENV_PATH', os.path.join(BASE_DIR, '.env')))
 SECRET_KEY = env('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['deezhistory.pythonanywhere.com']
 
