6c56ed20a8e8dd2877e34d90ebd72dc66df110f2
matthiasrsl <47864957+matthiasrsl@users.noreply.github.com>
2020-02-01 19:27:35 +0100

    Added Deezer accounts display on profile page - Also added link to link a Deezer account.

--- accounts/views.py
+++ accounts/views.py
@@ -3,10 +3,16 @@ from django.contrib.auth.decorators import login_required
 from django.contrib.auth.mixins import LoginRequiredMixin
 from django.views import View
 
+from django.conf import settings
+
 @login_required
 def display_profile(request):
     profile = request.user.profile
-    
+    deezer_accounts = profile.deezeraccount_set.all()
+    deezer_link_account_url = settings.DEEZER_OAUTH_URL.format(
+            settings.DEEZER_API_APP_ID, 
+            settings.DEEZER_AUTH_REDIRECT_URI
+    )
     return render(request, 'accounts/display_profile.html', locals())
     
     
