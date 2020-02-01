73ed75e69c363e030e100d34508e8890e5bd4b77
matthiasrsl <47864957+matthiasrsl@users.noreply.github.com>
2020-02-01 17:38:16 +0100

    Added method to retrieve Deezer user data.

--- accounts/models.py
+++ accounts/models.py
@@ -1,3 +1,5 @@
+import datetime as dt
+
 from django.db import models
 from django.contrib.auth.models import User
 from django.utils import timezone as tz
@@ -5,7 +7,7 @@ from django.conf import settings
 
 import requests
 
-from platform_apis.models import DeezerOAuthError
+from platform_apis.models import DeezerOAuthError, Market
 
 class Profile(models.Model):
     """
@@ -19,7 +21,7 @@ class Profile(models.Model):
         """
         Retrieves the user's access token from Deezer after authentication.
         """
-        url = 'https://connect.deezer.com/oauth/access_token.php'
+        url = settings.DEEZER_ACCESS_TOKEN_URL
         params = {
                 'app_id': settings.DEEZER_API_APP_ID,
                 'secret': settings.DEEZER_API_SECRET_KEY,
@@ -81,4 +83,45 @@ class DeezerAccount(PlatformAccount):
     explicit_content_level = models.CharField(max_length=100)
     flow_url = models.URLField(max_length=2000)
     
+    def update(self):
+        url = settings.DEEZER_API_USER_URL
+        params = {'access_token': self.access_token}
+        response = requests.get(url, params=params)
+        r_data = response.json()
+        
+        try:
+            error_type = r_data['error']['type']
+            message = r_data['error']['message']
+            code = r_data['error']['code']
+            raise DeezerApiError(error_type, message, code)
+        except KeyError:
+            pass  # No API-related error occured.
+            
+        self.user_id = r_data['id']
+        self.name = r_data['name']
+        self.lastname = r_data['lastname']
+        self.firstname = r_data['firstname']
+        self.email = r_data['email']
+        self.status = r_data['status']
+        if r_data['birthday'] != "0000-00-00":
+            birthday_list = r_data['birthday'].split('-')
+            birthday_list = [int(elt) for elt in birthday_list]
+            self.birthday = dt.date(*birthday_list)
+        inscription_date_list = r_data['inscription_date'].split('-')
+        inscription_date_list = [int(elt) for elt in inscription_date_list]
+        self.inscription_date = dt.date(*inscription_date_list)
+        self.gender = r_data['gender']
+        self.link = r_data['link']
+        self.picture_small = r_data['picture_small']
+        self.picture_medium = r_data['picture_medium']
+        self.picture_big = r_data['picture_big']
+        self.picture_xl = r_data['picture_xl']
+        market, created = Market.objects.get_or_create(code=r_data['country'])
+        market.save()
+        self.market = market
+        self.lang = r_data['lang']
+        self.is_kid = r_data['is_kid']
+        self.explicit_content_level = r_data['explicit_content_level']
+        self.flow_url = r_data['tracklist']
+        
     
