d07216d1082a4ee66f3f503dbb889292d357f4ab
matthiasrsl <matthias.roussel01@gmail.com>
2020-04-16 21:39:13 +0200

    Tweaked admin.

--- history/admin.py
+++ history/admin.py
@@ -4,8 +4,8 @@ from .models import *
 
 
 class HistoryEntryAdmin(admin.ModelAdmin):
-    list_display = ("profile", "track", "listening_datetime")
-    list_filter = ("profile",)
+    list_display = ("profile", "entry_type", "track", "listening_datetime")
+    list_filter = ("entry_type", "profile",)
     search_fields = ("track", "profile")
     date_hierarchy = "listening_datetime"
 
