5b114ad0c4536f7b193a7c0746a176bc9cb0066f
matthiasrsl <matthias.roussel01@gmail.com>
2020-04-25 16:05:54 +0200

    Deleted setting STATICFILES_DIRS for production.

--- muzhistory/settings.py
+++ muzhistory/settings.py
@@ -178,8 +178,6 @@ STATIC_URL = "/static/"
 
 STATIC_ROOT = os.path.join(BASE_DIR, "static")
 
-STATICFILES_DIRS = (os.path.join(BASE_DIR, "static"),)
-
 LOGIN_URL = "/accounts/login/"
 
 ADMIN_REORDER = (
