b3087818ad0dbb8fa28ef549f96dc8cd558e83b5
matthiasrsl <matthias.roussel01@gmail.com>
2020-05-02 16:50:40 +0200

    Finished player.

--- history/static/history/js/player.js
+++ history/static/history/js/player.js
@@ -7,42 +7,94 @@ function initPlayer() {
 }
 
 
-function play(id) {
-    var album_cover_url = document.querySelector("#track_" + id + " img").src;
-    var track_metadata = document.querySelector(
-        "#track_" + id + " .track_metadata"
-    ).cloneNode(true);
-    /* var track_title_short = document.querySelector(
-        "#track" + id + " .track_title"
-    ).firstChild;
-    var track_title_short = document.querySelector(
-        "#track" + id + " .track_title_version"
-    ).firstChild;
-    var artists = document.querySelector("#track" + id + " .artists").src;
-    var */
-    var player_album_cover = document.querySelector(
-        ".player .album_cover img"
-    );
-    var player_track_metadata = document.querySelector(
-        ".player .track_metadata"
-    );
-    player_album_cover.src = album_cover_url;
-    player_track_metadata.replaceWith(track_metadata);
-    var album_name = document.querySelector(".player .album_name");
-    if (album_name != null) {
-        document.querySelector(".player .additional_info").remove();
-    }
+function formatDuration(duration) {
+    return new Date(duration * 1000).toISOString().substr(14, 5);
+}
 
-    var allaudios = document.querySelectorAll('audio');
-    var audio = document.getElementById("audio_" + id);
-    if (audio.paused) {
-        for (var i = 0; i < allaudios.length; i++) {
-            allaudios[i].pause();
+function showPlayer() {
+    player = document.querySelector(".player");
+    player.style.bottom = "0";
+}
+
+function hidePlayer(event) {
+    player = document.querySelector(".player");
+    player.style.bottom = "-200px";
+}
+
+function play(id) {
+    showPlayer();
+    var audio = document.querySelector("audio.player_audio");
+    if (audio.dataset.currentlyPlayingId == id) {
+        if (audio.paused) {
+            audio.play();
+        } else {
+            audio.pause()
         }
-        audio.play();
     } else {
-        audio.pause();
-    }
+        var album_cover_url = document.querySelector("#track_" + id + " img").src;
+        var track_metadata = document.querySelector(
+            "#track_" + id + " .track_metadata"
+        ).cloneNode(true);
+        var duration = document.querySelector("#track_" + id).dataset.duration;
+        var audioSrc = document.querySelector("#track_" + id).dataset.audioSrc;
+
+        var player_album_cover = document.querySelector(
+            ".player .album_cover img"
+        );
+        var player_track_metadata = document.querySelector(
+            ".player .track_metadata"
+        );
+        var player_progress = document.querySelector(
+            ".player progress"
+        )
+        var total_duration_element = document.querySelector(
+            ".player #player_total_time"
+        ).firstChild;
+        var current_position_element = document.querySelector(
+            ".player #player_current_time"
+        ).firstChild;
+
+        player_album_cover.src = album_cover_url;
+        player_track_metadata.replaceWith(track_metadata);
+        var album_name = document.querySelector(".player .album_name");
+        if (album_name != null) {
+            document.querySelector(".player .additional_info").remove();
+        }
+        player_progress.max = duration;
+        total_duration_element.replaceWith(formatDuration(duration))
+        var begin_time = 30;
+        if (duration < 60 && duration >= 30) {
+            begin_time = duration-30;
+        } else if (duration < 30) {
+            begin_time = 0;
+        }
+        audio.dataset.beginTime = begin_time;
+        current_position_element.replaceWith(formatDuration(begin_time));
+        player_progress.value = begin_time;
+
+        audio.src = audioSrc;
+        audio.play();
+        audio.dataset.currentlyPlayingId = id;
+
+        audio.ontimeupdate = (event) => {updatePlayerProgress(event);}
+        audio.onended = (event) => {hidePlayer();};
+    }    
+}
+
+function updatePlayerProgress(event) {
+    var audio = document.querySelector("audio.player_audio");
+    begin_time = audio.dataset.beginTime;
+    var current_position_element = document.querySelector(
+        ".player #player_current_time"
+    ).firstChild;
+    var player_progress = document.querySelector(
+        ".player progress"
+    )
+    current_time = parseInt(begin_time) + parseFloat(audio.currentTime);
+    current_position_element.replaceWith(
+        formatDuration(current_time)
+    );
+    player_progress.value = current_time;
 }
 
 initPlayer();
