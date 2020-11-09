5ba899b1319759ca8e7df1438d80507554c377c7
matthiasrsl <matthias.roussel01@gmail.com>
2020-11-09 23:53:18 +0100

    Changed WIP notice on statistics page.

--- frontend/src/components/pages/StatsPage.js
+++ frontend/src/components/pages/StatsPage.js
@@ -1,9 +1,15 @@
 import React, { Component } from 'react';
 
+import "./StatsPage.css"
+
 
 class StatsPage extends Component {
     render() {
-        return (<div><h1>WIP</h1></div>)
+        return (
+            <div className="wip">
+                <h1>This page is not available yet.</h1>
+                <p>I'm working hard on it !</p>
+            </div>)
     }
 }
 
